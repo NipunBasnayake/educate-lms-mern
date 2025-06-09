4b8160c8cddd3ed716dc1b952bba9626a1cc15db
SahanChamara <sahanchamara456@gmail.com>
2025-06-09 15:21:58 +0530

    Implement API service and authentication features with error handling and response formatting

---
+++ client/src/redux/store-config/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import {useDispatch, useSelector} from "react-redux";
+import rootReducer from "./root-reducer";
+
+const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({
+        serializableCheck: true,
+        immutableCheck: true,
+    }),
+    devTools: import.meta.env.VITE_NODE_ENV !== "production",
+})
+
+export const useAppDispatch = () => useDispatch();
+export const useAppSelector = () => useSelector;
+
+export default store;
