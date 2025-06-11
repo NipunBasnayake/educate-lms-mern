a1fcb45be773c4a9ba9323d9561f0f5a78847aa9
Sahan Chamara <sahanchamara456@gmail.com>
2025-06-12 00:10:06 +0530

    feat: refactor layout component and enhance authentication handling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,34 +3,15 @@ import AppRoutes from "./routes/appRoute";
 import { useAppDispatch, useAppSelector } from "./redux/store-config/store";
 import { useEffect } from "react";
 import { refreshTokenAPI } from "./redux/features/authSlice";
+import Layout from "./components/Layout";
 
 function App() {
-  const dispatch = useAppDispatch();
-  const {isAuthenticated, loading, data} = useAppSelector((state) => state.auth);
-  const navigate = useNavigate();
-  const location = useLocation();
-  const lastPath = sessionStorage.getItem("lastPath");
-
-  useEffect(() => {
-    dispatch(refreshTokenAPI()).then(() => {
-      if(isAuthenticated && !loading){
-        const redirectPath = lastPath || "/dashboard";
-        navigate(redirectPath, {replace: true});
-      }
-    });
-  },[dispatch, isAuthenticated, loading,navigate]);
-
-  useEffect(() => {
-    if(isAuthenticated && !loading && location.pathname !== "/login"){
-      sessionStorage.setItem("lastPath", location.pathname);
-    }
-  },[location, isAuthenticated, loading]);
-
-  if(loading) return <div>Loading ...</div>
 
   return (
     <Router>
+      <Layout>
       <AppRoutes />
+      </Layout>
     </Router>
   );
 }
---
+++ client/src/components/Layout.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../redux/store-config/store";
+import { refreshTokenAPI } from "../redux/features/authSlice";
+
+const Layout = () => {
+  const dispatch = useAppDispatch();
+  const { isAuthenticated, loading, data } = useAppSelector(
+    (state) => state.auth
+  );
+
+  console.log("layout authnticate",isAuthenticated);
+  
+  const navigate = useNavigate();
+  const location = useLocation();
+  const lastPath = sessionStorage.getItem("lastPath");
+
+  useEffect(() => {
+    let isMounted = true;
+
+    if (!isAuthenticated && !loading) {
+      dispatch(refreshTokenAPI()).then(() => {
+        if (isAuthenticated && !loading) {
+          const redirectPath = lastPath || "/dashboard";
+          navigate(redirectPath, { replace: true });
+        }
+      });
+    }
+
+    return () => isMounted = false;
+    
+  }, [dispatch, isAuthenticated, loading, navigate, lastPath]);
+
+  useEffect(() => {
+    if (isAuthenticated && !loading && location.pathname !== "/login") {
+      sessionStorage.setItem("lastPath", location.pathname);
+    }
+  }, [location.pathname, isAuthenticated, loading]);
+
+  if (loading) return <div>Loading...</div>;
+
+  return <Outlet />; // Renders the matched route
+};
+
+export default Layout;
