1086683dd2eafae1372f3efc5bf80f5b9cdca39a
SahanChamara <sahanchamara456@gmail.com>
2025-06-16 11:33:18 +0530

    refactor: Enhance readability and logging in ProtectedRoute component

--- client/src/routes/protectedRoute.jsx
+++ client/src/routes/protectedRoute.jsx
@@ -9,31 +9,44 @@ const ProtectedRoute = ({ allowedRoles }) => {
     (state) => state.auth
   );
 
+  console.log("Protected Route auth", isAuthenticated, data);
+
   useEffect(() => {
     let isMounted = true;
     if (!isAuthenticated && !loading && !data) {
-      dispatch(refreshTokenAPI()).then(() => {
-        if (isMounted && !loading && !isAuthenticated) {
-          window.location.href = "/login";
-        }
-      });
+      dispatch(refreshTokenAPI())
+        .then(() => {
+          console.log("Refresh Token Attempt Completed");
+        })
+        .catch((err) => {
+          console.error("Refresh Token Failed", err);
+        })
+        .finally(() => {
+          if (isMounted && !loading && !isAuthenticated) {
+            window.location.href = "/login";
+          }
+        });
     }
 
-    return () => {isMounted = false;};
-  }, [dispatch,isAuthenticated,loading,data]);
+    return () => {
+      isMounted = false;
+    };
+  }, [dispatch, isAuthenticated, loading, data]);
 
-  if(loading) return null;
+  if (loading) return null;
 
   const hasAccess = isAuthenticated && (!allowedRoles || (data?.role && allowedRoles.includes(data.role)));
   console.log("has access", hasAccess);
-  
 
   return hasAccess ? (
-    <Outlet/>
+    <Outlet />
   ) : (
-    <Navigate to="/login" state={{message: error || "Please log in or check permissions." }} replace />
+    <Navigate
+      to="/login"
+      state={{ message: error || "Please log in or check permissions." }}
+      replace
+    />
   );
-  
 };
 
 export default ProtectedRoute;
