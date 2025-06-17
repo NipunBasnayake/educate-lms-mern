59b168e27f9174777c30399c6e1e793bcbf7ab8a
SahanChamara <sahanchamara456@gmail.com>
2025-06-17 15:22:05 +0530

    fix: extend refresh token cookie expiration to 24 hours

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -92,7 +92,7 @@ const register = async (req, res) => {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             sameSite: "strict",
-            maxAge: 15 * 60 * 1000,
+            maxAge: 24 * 60 * 60 * 1000,
         });
 
         res.cookie("refreshToken", refreshToken, {
