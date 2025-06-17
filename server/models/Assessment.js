2995a56450a8a997abff95b558823b12893774fd
RameshEdirisinghe <e2145434@gmail.com>
2025-06-17 12:04:19 +0530

    update schemas

--- server/models/Assessment.js
+++ server/models/Assessment.js
@@ -7,10 +7,14 @@ const Schema = mongoose.Schema;
 const AssessmentSchema = new Schema({
   title: { type: String, required: true },
   unit: { type: Schema.Types.ObjectId, ref: "Unit", required: true },
+  quizList: [{ type: Schema.Types.ObjectId, ref: "Quiz" }],
+  questionsCount: { type: Number, required: true },
+  duration: { type: Number, required: true },
+  passPercentage: { type: Number, required: true },
+  totalMarks: { type: Number, required: true },
   description: { type: String },
+  status: { type: String, enum: ["active", "inactive"], default: "active" },
   dueDate: { type: Date },
-  maxScore: { type: Number, required: true },
-  quizlist: [{ type: Schema.Types.ObjectId, ref: "Quiz" }],
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
 });
--- server/models/Quiz.js
+++ server/models/Quiz.js
@@ -8,7 +8,7 @@ const QuizSchema = new Schema({
     options: { type: [String], required: true },
     answer: { type: Number, required: true },
     unit: [{ type: Schema.Types.ObjectId, ref: 'Unit' }],
-    mark: { type: Number },
+    mark: { type: Number,required: true },
     createdAt: { type: Date, default: Date.now },
     updatedAt: { type: Date, default: Date.now }
 });
